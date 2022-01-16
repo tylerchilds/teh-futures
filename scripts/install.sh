083f5df810e15f8295105cc79c5d38906ab97724
Tyler Childs <email@tychi.me>
2022-01-16 15:16:17 -0800

    bugfix: split lock files for client and server

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-deno cache --lock=lock.json --lock-write public/editor.js
-deno cache --lock=lock.json --lock-write server.js
+deno cache --lock=client.lock.json --lock-write public/editor.js
+deno cache --lock=server.lock.json --lock-write server.js
